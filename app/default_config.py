bdfc93ce60b2f542e30ec3b29ff88525d92097c1
Kevin Roberts <kevin@nutech.com>
2017-10-11 09:45:06 -0700

    Default config

--- app/default_config.py
+++ app/default_config.py
@@ -2,15 +2,15 @@ import os
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
-ADMINS = frozenset(['general@support.nutech.com'])
-SECRET_KEY = 'Jim Swisher likes Ethers.'
+ADMINS = frozenset(['your@email.here'])
+SECRET_KEY = 'YOURSECRETKEYHERE'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DATABASE_CONNECT_OPTIONS = {}
 
 THREADS_PER_PAGE = 8
 WTF_CSRF_ENABLED = True
-WTF_CSRF_SECRET_KEY = 'jimswisherlovesethers.'
+WTF_CSRF_SECRET_KEY = 'somesecretkeyhere'
 
 BOOTSTRAP_SERVE_LOCAL = True
 
